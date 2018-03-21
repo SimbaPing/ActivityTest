734db723f439522aab069b87cd9e3193eaa38092
pjs <fpsping@163.com>
2018-03-21 15:55:02 +0800

    解决了 application 提示错误的问题

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -15,12 +15,15 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
 
+        // 增加的这个按钮点了之后发生些事情
         Button button1 = findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Toast 增加一个提示信息，点一下按钮提示，没毛病
 //                Toast.makeText(FirstActivity.this, "You click Button 1",
 //                Toast.LENGTH_SHORT).show();
+                // 显式 Intent，从第一页跳转到第二页，没毛病
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
