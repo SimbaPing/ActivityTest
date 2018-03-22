aa3a0a65a73f4d31c0a4e1a4905b784cf1509913
pjs <fpsping@163.com>
2018-03-22 23:08:17 +0800

    Intent 显式和隐式一般应用

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -4,12 +4,39 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
+
+    // 重写这个方法，创建了一个 menu，并允许创建的显示出来
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // 点击这两个按钮的时候，显示提示
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    // 关于按钮的一些东西
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -35,12 +62,30 @@ public class FirstActivity extends AppCompatActivity {
 //                Intent intent = new Intent(Intent.ACTION_DIAL);
 //                intent.setData(Uri.parse("tel:10086"));
 //                startActivity(intent);
-                // 5.
-                String data = "Hello SecondActivity";
+                // 5. 按理说应该在 logcat 返回一个字符串的值，但是没有
+                // 问题出在了华为手机，我用锤子测试过就没问题，华为是个坑
+//                String data = "Hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                intent.putExtra("extra_data", data);
+//                startActivity(intent);
+                // 6.1 点击一下跳转到2
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-                intent.putExtra("extra_data", data);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    // 6.2 接收按钮2返回过来的值
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,8 +14,22 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
 
-        Intent intent = getIntent();
-        String data = intent.getStringExtra("extra_data");
-        Log.d("SecondActivity", data);
+        // 对应 FirstActivity 中的5
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra_data");
+//        Log.d("SecondActivity", data);
+
+
+        // 隐式 Intent，点击按钮2，返回一个字符串，对应 FirstActivity 6
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
